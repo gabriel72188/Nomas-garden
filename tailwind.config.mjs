5b18f011f3acee3c6aee94b42bb21b3a22c3e68b
gabriel72188 <gabri72188340@gmail.com>
2026-03-22 18:08:11 +0100

    cambio de la tipografia, espaciado en los iconos de alergenos, el simbolo de euro y el recuadro de las imagenes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'bebas': ['"Bebas Neue"', 'sans-serif'],
+        'inco': ['"Inconsolata"', 'monospace'],
+      },
+    },
   },
   plugins: [],
 }
